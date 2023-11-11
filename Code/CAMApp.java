fb88ee195d2917ace7c930c68b5233b10b238b3b
Leong Wei Hong <42280031+weihonglwh@users.noreply.github.com>
2023-11-11 21:13:38 +0800

    Update CAMApp.java

--- Code/CAMApp.java
+++ Code/CAMApp.java
@@ -1,8 +1,6 @@
-import java.text.ParseException;
 import java.util.Scanner;
 import java.util.Objects;
 import java.util.Date;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class CAMApp {
